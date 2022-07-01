906c25626f67cd8319c212a290a06b720f24b267
Marek Pawlikowski <marek.pawlikowski87@gmail.com>
2022-07-01 15:30:17 +0200

    logs added

--- skrypt.sh
+++ skrypt.sh
@@ -8,11 +8,27 @@ echo $currentDate
 }
 
 
+logs()
+{
+currentDate=`date +%m-%d-%Y`
+
+for i in {1..100}; do
+	touch log${i}.txt;
+	echo log${i} >> log${i}.txt;
+	echo $0 >> log${i}.txt ;
+	echo $currentDate >> log${i}.txt;
+done
+
+   exit 0
+}
+
+
+
 
 for i in "$@"; do
   case $i in
     --data) data;;
-
+    --logs) logs;;
     *)
   esac
 done
